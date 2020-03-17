9172c39521fe75a63524216f8fd994174e7bd8bc
shrijan <shrijan00003@gmail.com>
2020-03-17 21:55:38 +0545

    - laravel is upgraded to 6.0 - helper is added as per upgrade - .old file for composer json to remember ckeditor is still missing (will remove after installing ckeditor) - php laravel websocket package install publish migration all added

--- config/app.php
+++ config/app.php
@@ -176,8 +176,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\ObserverServiceProvider::class,
-        Unisharp\Ckeditor\ServiceProvider::class,
+        App\Providers\ObserverServiceProvider::class
+
     ],
 
     /*
